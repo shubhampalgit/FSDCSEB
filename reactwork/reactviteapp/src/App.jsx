bb52c50cd401e661433fbb474901516ec021cb18
shubhampalgit <shubhampal201009@gmail.com>
2024-12-09 15:25:47 +0530

    9decreactwork

--- reactwork/reactviteapp/src/App.jsx
+++ reactwork/reactviteapp/src/App.jsx
@@ -1,87 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Student from './Student';
-import UseStudentState from "./UseStudentState"
+import UseStudentState from './UseStudentState';
 import Imagemanipulation from './Imagemanipulation';
-import UseFetchAPI from './UseFetchApi';
+// import MyUseEffect from '../MyUseEffect';
+import UseFetchAPI from './UseFetchAPI';
+import Login from './Login';
+import Registration from './Registration';
 function App() {
+
+  const[rdata,setrdata]=useState();
   const h1=<h1>Hello World</h1>;
  const mystyle={
   color:'red',
   backgroundColor:'yellow'
  }
+
+
  
-//  const studentdata =[{
-//   college:"ABES Engineering College",
-//   name:"Kuldeep",
-//   pic:<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>,
-//   branch:"CSE",
-//   roll:"230023947937",
-//   section :"B"
-//  },
-//  {
-//   college:"ABES Engineering College",
-//   name:"Deepak",
-//   pic:<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>,
-//   branch:"CSE",
-//   roll:"230023947937",
-//   section :"B"
-//  },
-//  {college:"ABES Engineering College",
-//  name:"Amit",
-//  pic:<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>,
-//  branch:"CSE",
-//  roll:"30023947937",
-//  section :"B"}]
   return (
-//     <div style={{backgroundColor:'cyan'}}>
-//       {h1}
-    
-//     <div style={mystyle}>
-//       ABES Engineering College
-//     </div>
-//     <div  style={{display:'flex'}}>
-//       {/* <Student data ={studentdata[1]}/> */
-//       studentdata.map(
-//         (ele)=>{
-//           return <Student data={ele}/>
-//         }
-//       )
-//       }
- 
-//       {/* <Student college="ABES Engineering College" 
-//       name="Rahul"
-//       branch="CSE"
-//       roll="2328392"
-//       section="A"
-      
-//       />
-
-// <Student college="ABES Engineering College" 
-//       name="Ankit"
-//       pic={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>}
-//       branch="CSE"
-//       roll="787878"
-//       section="A"
-      
-//       />
-
-//       <Student college="ABES Engineering College" 
-//       name="Amit Tomer"
-//       branch="CSE"
-//       roll="6754890"
-//       section="A"
-      
-//       /> */}
-//     </div>
-//     </div>
-  <div>
-    <h2>Hello using stateHook</h2>
     <div>
-      {/* <UseStudentState/> */}
-      {/* <Imagemanipulation/> */}
-      <UseFetchAPI/>
-    </div>
+      <div>
+        {JSON.stringify(rdata)}
+      </div>
+   <h2>Hello Using useSatte Hook</h2>
+  <div>
+    {/* <Login /> */}
+    <Registration regData={setrdata} />
+    {/* <UseStudentState /> */}
+    {/* <Imagemanipulation /> */}
+    {/* <MyUseEffect /> */}
+    {/* <UseFetchAPI /> */}
+    {/* <Login /> */}
+  <div>
+    <h2>Login Form</h2>
+    <Login regdata={rdata} />
   </div>
+   
+  </div>
+    </div>
+      
   )
 }
 
--- reactwork/reactviteapp/src/Registration.jsx
+++ reactwork/reactviteapp/src/Registration.jsx
@@ -1,8 +1,43 @@
-import Reac from 'react'
-function Registration(){
-    return(
-    <div>Registration</div>
-    )
-}
+import React, { useState } from 'react'
 
-export default Login
+function Registration({regData}) {
+const[userName,setUserName]=useState();
+const[email,setEmail]=useState();
+const[password,setPassword]=useState();
+const[userdata,setuserdata]=useState();
+
+  function showData(e){
+    e.preventDefault();
+    const data={
+      userName,
+      email,
+      password
+    }
+    regData(data);
+    //setuserdata(data);
+  }
+  return (
+    <div>
+      {/* <div>{JSON.stringify(userdata)}</div> */}
+         <form>
+         <div class="form-group">
+    <label for="exampleInputEmail1">Name</label>
+    <input onChange={(e)=>setUserName(e.target.value)} type="text"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name" />
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input onChange={(e)=>setEmail(e.target.value)} type="email"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input onChange={(e)=>setPassword(e.target.value)} type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+  </div>
+  
+  <button type="submit" onClick={showData} class="btn btn-primary">Register</button>
+</form>
+    </div>
+  )
+}
+export default Registration
